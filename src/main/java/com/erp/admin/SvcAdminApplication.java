313d76de69d2f0815cbaaacbaf02b6be63798824
rajsonam1986 <rajsonam1986@gmail.com>
2023-05-20 12:45:34 -1200

    initial comiit

---
+++ src/main/java/com/erp/admin/SvcAdminApplication.java
@@ -0,0 +1,13 @@
+package com.erp.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/admin/SvcAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
